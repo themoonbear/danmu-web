88e9fc7cda732a828104ab666960c8514a373462
sean <devman@foxmail.com>
2018-10-09 20:09:38 +0800

    初始化工程

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app dark>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title"/>
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container 
+        fluid 
+        fill-height>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      :right="right"
+      v-model="rightDrawer"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-subheader 
+          two-line>已支持直播平台</v-subheader>
+        <v-list-tile 
+          v-for="item in platforms" 
+          :key="item">
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item"/>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-footer
+        height="auto"
+        absolute
+        color="blue">
+        <v-layout
+          row
+          justify-center>
+          <v-btn
+            flat
+            href="https://github.com/themoonbear/danmu-web/issues/new">
+            <span>问题反馈</span>
+          </v-btn>
+        </v-layout>      
+      </v-footer>    
+    </v-navigation-drawer>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>Copyright &copy; 2018 Moonbear Studio</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      right: true,
+      rightDrawer: false,
+      title: 'OBS 弹幕云插件',
+      platforms: ['B站'],
+      bottomNav: 'feedback'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+    justify-center
+    align-center>
+    <v-flex
+      xs12
+      sm8
+      md6>
+      <v-card class="elevation-12">
+        <v-card-title class="headline font-weight-regular blue white--text">直播地址</v-card-title>
+        <v-card-text>
+          <v-text-field
+            box
+            placeholder="输入后回车提交"
+            hint="如：https://live.bilibili.com/123456"
+            @keyup.enter="submit"/>          
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  methods: {
+    submit(event) {
+      alert(event.target.value)
+    }
+  }
+}
+</script>
